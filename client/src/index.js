1bf22929bca447f5fdd5fef19dead1057b29e50f
Casey White <caseywhite534@gmail.com>
2019-01-08 06:33:12 -0800

    simple home screen

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import HomeScreen from './components/HomeScreen/HomeScreen'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<HomeScreen />, document.getElementById('root'))
 registerServiceWorker()
