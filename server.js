6024f1e9d619a50e4395f25a0026f54ebed0a420
Cameron Osborn <cameron0401@gmail.com>
2019-01-25 16:33:52 -0800

    Restructure paths and change to mongodb service

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
+// var {mongoose} = require('./db/mongoose')
+var { User } = require('./models/user')
+
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// API calls
-app.get('/api/getData', require('./api/getData').getAllData)
+// API routes
+app.post('/users', (req, res) => {
+  var user = new User({
+    email: req.body.text
+  })
+
+  user.save().then((doc) => {
+    res.send(doc)
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
 
 if (isProduction) {
   console.log('production')
