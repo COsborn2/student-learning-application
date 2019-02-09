95266d8bd3854db0ab3d2e68fe57b697884ae7a8
Cameron Osborn <cameron0401@gmail.com>
2019-02-08 17:32:32 -0800

    Refactor glob pattern again. Fix master files to follow standard

--- client/src/components/students/spelling/DropZone.js
+++ client/src/components/students/spelling/DropZone.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import { DropTarget } from 'react-dnd';
+import React from 'react'
+import { DropTarget } from 'react-dnd'
 
-var dropZoneID = null;
+var dropZoneID = null
 
 const Types = {
   SPELLINGCARD: 'spellingCard'
 }
 
 const dropTarget = {
-  drop(props, monitor){
-    const item = monitor.getItem();
-    props.parentTest(dropZoneID, item.value);
+  drop (props, monitor) {
+    const item = monitor.getItem()
+    props.parentTest(dropZoneID, item.value)
   }
 }
 
-function collect(connect, monitor)
-{
-  return{
+function collect (connect, monitor) {
+  return {
     connectDropTarget: connect.dropTarget(),
     isOver: monitor.isOver()
-  };
+  }
 }
 
-function DropZone(props)
-{
-  const {id, value} = props;
-  const {isOver, connectDropTarget} = props;
-  dropZoneID = id;
-
+function DropZone (props) {
+  const { id, value } = props
+  const { connectDropTarget } = props
+  dropZoneID = id
 
   return connectDropTarget(
-    <div key={"dropzone" + id}
-    className='col-md-1 mx-1 card badge-warning'>[{id}][{value}]Drag a letter here!</div>)
+    <div key={'dropzone' + id}
+      className='col-md-1 mx-1 card badge-warning'>[{id}][{value}]Drag a letter here!</div>)
 }
 
-export default DropTarget(Types.SPELLINGCARD, dropTarget, collect)(DropZone);
+export default DropTarget(Types.SPELLINGCARD, dropTarget, collect)(DropZone)
