e630bc1c94d1418c0211d1d0a3694ea0b5cc8fa1
Casey White <caseywhite534@gmail.com>
2019-02-18 20:32:00 -0800

    Moves all logic from InstructorHome to InstructorView

--- client/src/components/instructor/InstructorView.js
+++ client/src/components/instructor/InstructorView.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { Redirect, Route } from 'react-router-dom'
 import InstructorHome from './InstructorHome'
 import InstructorApiCalls from '../../javascript/InstructorApiCalls'
+import Course from './Course'
+import Button from 'react-bootstrap/Button'
 
 /* The instructor view manages all screens and routes for a specific instructor user
  the login screen creates and authenticates an instructor object, and passes it
@@ -17,8 +19,30 @@ class InstructorView extends Component {
       id: user.id,
       jwt: user.jwt,
       api: InstructorApiCalls,
-      courses: null
+      courses: null,
+      selectedCourse: -1
     }
+    this.onCourseClick = this.onCourseClick.bind(this)
+  }
+
+  onCourseClick (index) {
+    let { selectedCourse } = this.state
+    selectedCourse = index === selectedCourse ? -1 : index
+    this.setState({ selectedCourse })
+  }
+
+  createCourseComponents () {
+    return (
+      <div>
+        {this.state.courses.map((course, index) =>
+          <div key={index}>
+            <Button onClick={() => this.onCourseClick(index)} className='test btn-lg btn-primary rounded-pill'>{course.className}</Button>
+            <Course {...this.props} show={index === this.state.selectedCourse} course={course} />
+            <hr />
+          </div>
+        )}
+      </div>
+    )
   }
 
   componentDidMount () {
@@ -32,10 +56,17 @@ class InstructorView extends Component {
   }
 
   render () {
-    let { jwt, courses } = this.state
+    let { jwt, courses, id } = this.state
     if (!jwt) return <Redirect to='/login/instructor' />
     if (!courses) return <div />
-    return <Route path='/instructor/:userId' render={(props) => <InstructorHome {...props} courses={courses} />} />
+    return (
+      <div className='container text-center'>
+        <header className='jumbotron my-3 bg-info'>
+          <h1 className='display-4 font-weight-bold'> Hello {id}</h1>
+        </header>
+        {this.createCourseComponents()}
+      </div>
+    )
   }
 }
 
