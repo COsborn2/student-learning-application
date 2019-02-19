181d506d90a734f9e37039385211f16e63346111
Casey White <caseywhite534@gmail.com>
2019-02-18 20:05:37 -0800

    Refactored instructor api calls to be a static class

---
+++ client/src/javascript/InstructorApiCalls.js
@@ -0,0 +1,31 @@
+class InstructorApiCalls{
+
+  // This is where backend api call is made
+  verifyAuth (id, pass) {
+    if (id === 'instructorDev' && pass === 'password') { // this is just for the devSkip button
+      return 'Valid JWT'
+    }
+    console.log('Expected instructor api login call.')// todo
+    return null
+  }
+
+  // This is where backend api call is made
+  verifySignup (id, pass) {
+    if (id.length > 3 && pass.length > 3) {
+      return 'Valid JWT'
+    }
+    console.log('Expected instructor api sign-up call.')// todo
+    return null
+  }
+
+  // This is where the api call is made to retrieve the specific instructor's classes
+  updateClasses () {
+    let classrooms = [{
+      classCode: 1,
+      students: [{ userName: 'devStudent' }]
+    }]
+    return classrooms
+  }
+}
+
+export default InstructorApiCalls
