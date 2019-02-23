b7a9b6826fb071a1f0b4579472cba7ce7c11b5f1
Trent P <trent.parkey@gmail.com>
2019-02-22 20:57:52 -0800

    Started to work time tracking, although it is not complete yet and thus doesnt work. Word order tracking and incorrect attempts tacking has been added as well.

--- client/src/components/students/StudentSpelling.js
+++ client/src/components/students/StudentSpelling.js
@@ -3,6 +3,7 @@ import { Image } from 'react-bootstrap'
 import './StudentSpelling.css'
 import SpellingCard from './spelling/SpellingCard.js'
 import DropZone from './spelling/DropZone.js'
+import Timer from './spelling/Timer.js'
 import PropTypes from 'prop-types'
 import ItemPreview from './spelling/ItemPreview.js'
 import { DragDropContext } from 'react-dnd'
@@ -45,6 +46,17 @@ function initializeDropZone (numCharsInWord) {
   return dropZone
 }
 
+function calculateWrongMove(dropOrder, wordToSpell)
+{
+  let count = 0;
+  for(let i = 0; i < dropOrder.length; i = i + 2)
+  {
+    if(dropOrder[i+1] !== wordToSpell[dropOrder[i]])
+      count ++;
+  }
+  return count
+}
+
 /*
   We pass an array of wordObjects as a property. Each item in the array consists of a word, and an imageURL
   You can access them like shown below. When a word is completed move on to the next word.
@@ -62,13 +74,14 @@ class StudentSpelling extends React.Component {
       curWordToSpell: firstWordToSpell,
       curImageURL: wordsToSpell[0].imageURL,
       curHand: getLetters(firstWordToSpell),
-      curDropZone: initializeDropZone(firstWordToSpell.length)
+      curDropZone: initializeDropZone(firstWordToSpell.length),
+      dropOrder: []
     }
     this.advanceToNextWord = this.advanceToNextWord.bind(this)
   }
 
   advanceToNextWord () {
-    let { wordsToSpell, wordIndex, curWordToSpell, curImageURL, curHand, curDropZone } = this.state
+    let { wordsToSpell, wordIndex, curWordToSpell, curImageURL, curHand, curDropZone, dropOrder } = this.state
     const allWordsSpelled = wordIndex >= wordsToSpell.length - 1
     wordIndex++
     if (!allWordsSpelled) {
@@ -77,30 +90,34 @@ class StudentSpelling extends React.Component {
       curImageURL = nextWordItem.imageURL
       curHand = getLetters(curWordToSpell)
       curDropZone = initializeDropZone(curWordToSpell.length)
-      this.setState({ wordsToSpell, wordIndex, curWordToSpell, curImageURL, curHand, curDropZone })
+      dropOrder = []
+      this.setState({ wordsToSpell, wordIndex, curWordToSpell, curImageURL, curHand, curDropZone, dropOrder })
     }
     this.props.onWordCompletion(wordIndex, allWordsSpelled)
   }
 
   renderButton (isSpelled) {
-    const buttonStyle = 'btn btn-' + (isSpelled ? 'success' : 'secondary')
+    const buttonStyle = 'mx-auto btn btn-' + (isSpelled ? 'success' : 'secondary')
     return <button type='button' className={buttonStyle} onClick={this.advanceToNextWord}
       disabled={!isSpelled}>Continue</button>
     // return <button type='button' className='btn btn-danger' onClick={this.onResetClick}>Reset</button>
   }
 
   setDropZone = (dropZoneID, letterDropped, cardID) => {
-    let { curDropZone, curHand, curWordToSpell } = this.state
+    let { curDropZone, curHand, curWordToSpell, dropOrder } = this.state
     const expectedLetter = curWordToSpell[dropZoneID]
+
+    dropOrder.push(dropZoneID)
+    dropOrder.push(letterDropped)
 
     if (curDropZone[dropZoneID] !== expectedLetter) curDropZone[dropZoneID] = letterDropped
     if (curDropZone[dropZoneID] === expectedLetter && letterDropped === expectedLetter) curHand.splice(cardID, 1)
 
-    this.setState({ curDropZone, curHand })
+    this.setState({ curDropZone, curHand, dropOrder })
   }
 
   render () {
-    const { curHand, curDropZone, curWordToSpell } = this.state
+    const { curHand, curDropZone, curWordToSpell, dropOrder } = this.state
     const isSpelled = isWordSpelled(curDropZone, curWordToSpell)
     const button = this.renderButton(isSpelled)
     const status = getStatus(isSpelled, curWordToSpell)
@@ -124,15 +141,24 @@ class StudentSpelling extends React.Component {
         <div className='row'>
           {letterCards}
         </div>
-        {button}
+        <div className='row'>
+          {button}
+        </div>
         <ItemPreview key='__preview' name='Item' />
+        <span>Stat zone</span>
+        <div className='mx-auto'>
+          Total Time: <Timer time = {this.state.endTime}/>
+          dropOrder: {dropOrder.toString()}
+          Wrong Moves: {calculateWrongMove(dropOrder, curWordToSpell)}
+        </div>
       </div>)
   }
 }
 
 StudentSpelling.propTypes = {
   wordsToSpell: PropTypes.array.isRequired,
-  onWordCompletion: PropTypes.func.isRequired
+  onWordCompletion: PropTypes.func.isRequired,
+  totalTime: PropTypes.number
 }
 
 export default DragDropContext(TouchBackend({ enableMouseEvents: true }))(StudentSpelling)
