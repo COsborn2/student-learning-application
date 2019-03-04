2c1e626f26abf4f9b982c3e18a01f87fb77faa40
Casey White <caseywhite534@gmail.com>
2019-03-04 02:05:22 -0800

    Simple loading spinner for instructor view

---
+++ client/src/components/helpers/LoadingSpinner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function LoadingSpinner (props) {
+  let loadingStyle = ''
+  if (props.isLoading) {
+    loadingStyle = 'loading-img'
+  } else {
+    loadingStyle = 'loading-img-finish'
+    setTimeout(() => props.onLoadingAnimComplete(), 1000)
+  }
+  return (
+    <img className={loadingStyle}
+      src='https://media0.giphy.com/media/5AtXMjjrTMwvK/giphy.gif?cid=3640f6095c7cdfe533726d36630279e9'
+      alt='Loading' />)
+}
+
+LoadingSpinner.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onLoadingAnimComplete: PropTypes.func.isRequired
+}
+
+export default LoadingSpinner
