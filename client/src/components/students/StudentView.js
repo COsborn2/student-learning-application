01d7b7199f9c37767a0dca5c9bb78d3387929d3d
Casey White <caseywhite534@gmail.com>
2019-03-05 00:57:30 -0800

    Implemented a mock/implemented hybrid of api calls made on startup

--- client/src/components/students/StudentView.js
+++ client/src/components/students/StudentView.js
@@ -19,58 +19,57 @@ class StudentView extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      id: this.props.id,
+      username: this.props.id,
       jwt: this.props.jwt,
+      classCode: null,
       assignments: null,
+      currentAssignment: null,
       progress: null,
       isLoadAnimComplete: false
     }
     this._isLoading = true
     this.onWordCompletion = this.onWordCompletion.bind(this)
-    this.onLoadingAnimComplete = this.onLoadingAnimComplete.bind(this)
+    this.onLoadingComplete = this.onLoadingComplete.bind(this)
   }
 
   async componentDidMount () {
     let { jwt } = this.state
-    // const res = await StudentApiCalls.getAssignmentsAndProgress(jwt)
-    // const student = res.student
-    //    let progress = {
-    //      currentAssignment: student.currentAssignment,
-    //      currentLetter: student.currentLetter,
-    //      currentWord: student.currentWord,
-    //      finishedCourse: student.finishedCourse
-    //    }
-    //    const classroom = res.classroom
-    //    console.log(student)
-    //    console.log(classroom)
-
-    let assignments = StudentApiCalls.getAssignments(jwt)
-    let progress = StudentApiCalls.getProgress(jwt)
-    if (assignments && progress) {
-      setTimeout(() => {
-        this._isLoading = false
-        this.setState({ assignments, progress })
-      }, 1000)
+    const res = await StudentApiCalls.getAssignmentsAndProgress(jwt)
+    const student = res.student
+    const progress = {
+      currentAssignmentIndex: 0, // student.currentAssignment, todo remove mock
+      currentLetterIndex: student.currentLetter,
+      currentWordIndex: student.currentWord,
+      finishedCourse: student.finishedCourse
     }
-    setTimeout(() => {
+    const classroom = res.classroom
+    const classCode = classroom.classcode
+    const classAssignments = classroom.assignments
+    const currentAssignment = await StudentApiCalls.getAssignmentById(progress.currentAssignmentIndex)
+    const assignments = StudentApiCalls.getAssignments(jwt) // todo remove mocked assignments when a route is created for the letter bar
+
+    console.log(student)
+    console.log(classAssignments)
+
+    if (student && classroom) {
       this._isLoading = false
-      this.setState({ assignments: [] })
-    }, 1000)
+      this.setState({ assignments, progress, classCode, currentAssignment })
+    }
   }
 
-  onLoadingAnimComplete () {
+  onLoadingComplete () {
     this.setState({ isLoadAnimComplete: true })
-    this.props.history.replace(`/student/${this.state.id}`) // todo remove this. Handle redirection in authenticatedRoute
+    this.props.history.replace(`/student/${this.state.username}`) // todo remove this. Handle redirection in authenticatedRoute
   }
 
   onWordCompletion (wordIndex, allWordsSpelled) {
-    let { id, jwt, api, progress } = this.state
-    progress.curWordIndex = wordIndex
+    let { username, jwt, api, progress } = this.state
+    progress.currentWordIndex = wordIndex
     if (allWordsSpelled) {
-      progress.curWordIndex = 0 // todo this is temporary
+      progress.currentWordIndex = 0 // todo this is temporary
       console.log('All words have been spelled. For now the words will repeat')
       this.setState({ progress })
-      this.props.history.push(`/student/${id}`)
+      this.props.history.push(`/student/${username}`)
     }
     api.putAssignments(jwt, progress)
   }
@@ -81,22 +80,21 @@ class StudentView extends Component {
   }
 
   render () {
-    const { assignments, progress, isLoadAnimComplete } = this.state
-    if (!isLoadAnimComplete) return <LoadingSpinner isLoading={this._isLoading} onLoadingAnimComplete={this.onLoadingAnimComplete} />
-    let wordsToSpell = assignments[progress.curAssignmentIndex].words
-
+    const { currentAssignment, assignments, progress, isLoadAnimComplete } = this.state
+    if (!isLoadAnimComplete) return <LoadingSpinner isLoading={this._isLoading} onLoadingAnimComplete={this.onLoadingComplete} />
+    console.log(currentAssignment)
     return (
       <div style={{ background: '#a9a9a9' }}>
         <Switch>
-          <Route exact path='/student/:id' render={(props) => <StudentHome {...props} assignments={assignments} progress={progress} />} />
-          <Route path='/student/:id/writing' component={StudentWriting} />
-          <Route path='/student/:id/spelling' render={() =>
+          <Route exact path='/student/:username' render={(props) => <StudentHome {...props} assignments={assignments} progress={progress} />} />
+          <Route path='/student/:username/writing' component={StudentWriting} />
+          <Route path='/student/:username/spelling' render={() =>
             <DragDropContextProvider
               backend={TouchBackend}>  { /* this needed to be moved up from StudentSpelling because advancing to the next word would cause multiple HTML5Backend's to be instantiated */}
-              <StudentSpelling wordsToSpell={wordsToSpell}
+              <StudentSpelling wordsToSpell={currentAssignment.words}
                 onWordCompletion={(wordIndex, allWordsSpelled) => this.onWordCompletion(wordIndex, allWordsSpelled)} />
             </DragDropContextProvider>} />
-          <Route path='/student/:id/video' component={StudentVideo} />
+          <Route path='/student/:username/video' component={StudentVideo} />
         </Switch>
       </div>
     )
