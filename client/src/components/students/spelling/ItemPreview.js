f6dae274ab60daeaedf12810a4138b382ad4ef04
Casey White <caseywhite534@gmail.com>
2019-03-08 03:16:36 -0800

    Fixed import

--- client/src/components/students/spelling/ItemPreview.js
+++ client/src/components/students/spelling/ItemPreview.js
@@ -5,7 +5,7 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import DragLayer from 'react-dnd/lib/DragLayer'
+import { DragLayer } from 'react-dnd'
 
 function collect (monitor) {
   var item = monitor.getItem()
