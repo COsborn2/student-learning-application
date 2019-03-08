de59d9487db9f11ea1234afc540ee8486f550c95
Trent P <trent.parkey@gmail.com>
2019-03-07 17:01:42 -0800

    Forgot to add fixed standard

--- client/src/components/helpers/LandScape.js
+++ client/src/components/helpers/LandScape.js
@@ -3,12 +3,11 @@ import React from 'react'
 const centerStyle = { top: 0, bottom: 0, left: 0, right: 0 }
 
 function LandScape (props) {
-    if ( props.isLandScape) {
-        return <div style={centerStyle}> Please LandScape </div>
-    }
-    else {
-        return <div></div>
-    }
+  if (props.isLandScape) {
+    return <div style={centerStyle}> Please LandScape </div>
+  } else {
+    return <div />
+  }
 }
 
 export default LandScape
