c8fc9ca280bcce112e5f609d30bfe51a39f0b858
Casey White <caseywhite534@gmail.com>
2019-03-10 03:32:09 -0700

    Implemented update api call, and commented student api

--- client/src/javascript/StudentApiCalls.js
+++ client/src/javascript/StudentApiCalls.js
@@ -5,6 +5,7 @@ const signupURL = '/api/student'
 const loginURL = '/api/student/login'
 const initURL = '/api/student'
 const getAssignmentURL = '/api/assignment/'
+const updateProgressURL = '/api/student/progress'
 const detectWritingURL = '/api/student/writing'
 
 async function stall (stallTime = 3000) {
@@ -12,6 +13,12 @@ async function stall (stallTime = 3000) {
 }
 
 class StudentApiCalls {
+  /***
+   * This method calls the signup api.
+   * @param classCode classcode used to signup
+   * @param username username used to signup
+   * @returns {Promise<*>} returns the user
+   */
   static async signup (classCode, username) {
     let httpMessage = {
       method: 'POST',
@@ -47,6 +54,12 @@ class StudentApiCalls {
     }
   }
 
+  /***
+   * This method calls the login api.
+   * @param classCode classcode used to login
+   * @param username username used to login
+   * @returns {Promise<*>} returns the user
+   */
   static async login (classCode, username) {
     let httpMessage = {
       method: 'POST',
@@ -82,6 +95,11 @@ class StudentApiCalls {
     }
   }
 
+  /***
+   * This method calls the init student api.
+   * @param jwt Web token
+   * @returns {Promise<*>} AssignmentIds and populated letters
+   */
   static async getInitialStudentState (jwt) {
     let httpMessage = {
       method: 'GET',
@@ -103,6 +121,11 @@ class StudentApiCalls {
     return body
   }
 
+  /***
+   * This method calls the get assignment api
+   * @param id Id to get
+   * @returns {Promise<*>} Populated assignment
+   */
   static async getAssignmentById (id) {
     let httpMessage = {
       method: 'GET',
@@ -122,6 +145,12 @@ class StudentApiCalls {
     return body.assignment
   }
 
+  /***
+   * This method is used to call the detectWriting api.
+   * @param jwt Web token
+   * @param image64 Base 64 encoded image
+   * @returns {Promise<*>}
+   */
   static async detectWriting (jwt, image64) {
     let httpMessage = {
       method: 'POST',
@@ -142,6 +171,55 @@ class StudentApiCalls {
     }
 
     return body
+  }
+
+  /***
+   * This method is used to call the update student progress route
+   * @param jwt Web token
+   * @param progress Current students progress
+   * @returns {Promise<*>} Returns the updated student
+   */
+  static async updateStudentProgress (jwt, progress) {
+    let httpMessage = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth': jwt
+      },
+      body: JSON.stringify({ progress })
+    }
+
+    const res = await fetch(updateProgressURL, httpMessage)
+    const body = await res.json()
+    if (res.status !== 200) {
+      console.log(httpMessage) // todo remove log statements
+      console.log(res)
+      console.log(`Error: ${body.error}`)
+      return { error: body.error }
+    }
+
+    return {
+      username: body.username,
+      jwt,
+      classcode: body.classcode,
+      progress: {
+        currentAssignmentIndex: body.currentAssignment,
+        currentWordIndex: body.currentLetter, // if word index is equal to the array size, all words have been spelled
+        currentLetterIndex: body.currentWord,
+        finishedCourse: body.finishedCourse
+      }
+    }
+  }
+
+  /***
+   * This method uses local storrage rather than calling the api. Will be removed when api is fixed
+   * @param jwt Web token
+   * @param progress Current users progress
+   * @returns {Promise<boolean>} returns true if successful
+   */
+  static async updateStudentProgressMock (jwt, progress) {
+    window.localStorage.setItem('progress', JSON.stringify(progress))
+    return true
   }
 
   static async getAssignmentByIdMock (id) {
