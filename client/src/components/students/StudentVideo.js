99142b1aff26a50b3a20cac055bde79d7cf42246
Cameron Osborn <cameron0401@gmail.com>
2019-03-11 17:27:26 -0700

    Make StudentVideo accept params

--- client/src/components/students/StudentVideo.js
+++ client/src/components/students/StudentVideo.js
@@ -1,17 +1,15 @@
 import React, { PureComponent } from 'react'
-
-function getVideo () { // TODO: pass video url to this component
-  return 'https://www.youtube.com/embed/36IBDpTRVNE'
-}
+import PropTypes from 'prop-types'
 
 class StudentVideo extends PureComponent {
   render () {
+    const { video } = this.props
     return (
       <div className='mx-auto text-center align-middle' style={{ background: '#b9d5e0', marginTop: '0', paddingTop: '2%', width: '90%' }}>
         <div className='mx-auto' style={{ width: '55%', paddingBottom: '2%' }}>
           <div className='iframe-container' style={{ background: '#4085bd', width: '100%', padding: '3%', boxShadow: '10px 10px 5px 1px #6b6b6b' }}>
             <div className='iframe-container' style={{ position: 'relative', width: '100%', paddingBottom: '56.25%', height: '0' }}>
-              <iframe title='LetterVideo' width='560' height='315' src={getVideo()} frameBorder='0' allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
+              <iframe title='LetterVideo' width='560' height='315' src={video} frameBorder='0' allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
                 style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }} />
             </div>
           </div>
@@ -19,6 +17,10 @@ class StudentVideo extends PureComponent {
       </div>
     )
   }
+}
+
+StudentVideo.propTypes = {
+  video: PropTypes.object.isRequired
 }
 
 export default StudentVideo
