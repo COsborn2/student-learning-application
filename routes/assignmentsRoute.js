fddf421c36cb443ed35a1632dc88ba5f2ac7bd32
Cameron Osborn <cameron0401@gmail.com>
2019-03-19 18:14:19 -0700

    Configure ApiDoc. Create doc for GetAssignment

--- routes/assignmentsRoute.js
+++ routes/assignmentsRoute.js
@@ -1,7 +1,48 @@
 const { Assignment } = require('../models/assignment')
 const { WarningMessage, SuccessMessage } = require('../middleware/message')
 
-// /api/assignment/id
+/**
+ * @api {get} /api/assignment/:id Get Assignment Information
+ * @apiVersion 0.9.0
+ * @apiName GetAssignment
+ * @apiGroup Assignment
+ *
+ * @apiParam {Number} id Assignment ObjectId
+ *
+ * @apiSuccess {String} assignment Assignment object with [videos], [letters],
+ *    [words], _id, name. Words is populated
+ *
+ * @apiSuccessExample Success-Response:
+ *    {
+ *      "assignment": {
+ *        "videos": [],
+ *        "letters": [
+ *          "a",
+ *          "b",
+ *          "c"
+ *        ],
+ *        "words": [
+ *          {
+ *            "_id": "<id>",
+ *            "text": "cab",
+ *            "picture": "<url>",
+ *            "__v": 0
+ *          }
+ *        ],
+ *        "_id": "<id>",
+ *        "name": "Assignment 1",
+ *        "__v": 0
+ *      }
+ *    }
+ *
+ * @apiError (404) IdNotFound The id of that Assignment was not found
+ *
+ * @apiErrorExample
+ *    {
+ *      "error": "Assignment with that id of (<id>) could not be found"
+ *    }
+ *
+ */
 let getAssignmentById = async (req, res) => {
   let assignmentId = req.params.id
 
